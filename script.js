4a00413dc316fe3868faec0f15027b5027c32010
crappyartist <crappy.artist@mail.com>
2025-05-04 11:16:55 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -86,3 +86,52 @@ document.querySelectorAll('a').forEach(link => {
     });
   });
   
+  function copyText(btn) {
+    // copy the text
+    navigator.clipboard.writeText(btn.textContent.trim());
+    // swap the icon
+    btn.classList.add('copied');
+    // after 2s, revert back
+    setTimeout(() => btn.classList.remove('copied'), 1000);
+  }
+
+
+  document.querySelectorAll('.imgholder img').forEach(img => {
+    img.addEventListener('contextmenu', e => e.preventDefault());
+  });
+
+/*-------------------------------------------------------------------------------------------------------------gallery functionality--*/
+const galleryContainers = document.querySelectorAll('.gallery-container');
+        
+// Add event listeners to each gallery
+galleryContainers.forEach(container => {
+    const galleryRow = container.querySelector('.gallery-row');
+    const prevButton = container.querySelector('.gallery-button.prev');
+    const nextButton = container.querySelector('.gallery-button.next');
+    
+    // Scroll amount for each click (can be adjusted)
+    const scrollAmount = 700;
+
+    document.querySelectorAll('.gallery-button.prev').forEach(button => {
+    button.style.setProperty('display', 'none');
+    });
+
+    // Previous button click handler
+    prevButton.addEventListener('click', () => {
+        galleryRow.scrollBy({
+            left: -scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+    
+    // Next button click handler
+    nextButton.addEventListener('click', () => {
+        galleryRow.scrollBy({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+        document.querySelectorAll('.gallery-button.prev').forEach(button => {
+          button.style.setProperty('display', 'block');
+          });
+    });
+});
